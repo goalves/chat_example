02cfff1cbed53706f2412cf4734b076cecac1bbc
Gabriel Alves <g04lves@gmail.com>
2021-12-03 10:23:52 -0300

    feat: render messages on chat liveview

--- lib/ambush_chat_web/live/chat_live.ex
+++ lib/ambush_chat_web/live/chat_live.ex
@@ -1,3 +1,11 @@
 defmodule AmbushChatWeb.ChatLive do
   use Phoenix.LiveView
+
+  alias AmbushChat.Message
+
+  def mount(_params, _session, socket) do
+    messages = [Message.new("oi"), Message.new("tudo bem?")]
+
+    {:ok, assign(socket, :messages, messages)}
+  end
 end
