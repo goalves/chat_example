f538224fe419ab0bcd057579bfc47a7a0e4f77f0
Gabriel Alves <g04lves@gmail.com>
2021-12-03 09:54:26 -0300

    feat: move to using leex file instead of implementing render function

--- lib/ambush_chat_web/live/chat_live.ex
+++ lib/ambush_chat_web/live/chat_live.ex
@@ -1,8 +1,3 @@
 defmodule AmbushChatWeb.ChatLive do
   use Phoenix.LiveView
-
-  def render(_assigns) do
-    messages = ["oi", "tudo bem?"]
-    AmbushChatWeb.PageView.render("chat.html", messages: messages)
-  end
 end
